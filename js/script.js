169ae39a7f91b4748b01ed7c29e713b027daa3bd
Mohamed Khidir Ageeb <ageeb1982@gmail.com>
2021-12-01 15:10:52 +0300

    add figh part1

---
+++ js/script.js
@@ -0,0 +1,6 @@
+import Quran from './books/quran.js';// console.log(Quran());
+import Hadith from './books/hadith.js';// console.log(Quran());
+import Aqida from './books/aqida.js';// console.log(Quran());
+console.log(Quran());
+console.log(Hadith());
+console.log(Aqida());
