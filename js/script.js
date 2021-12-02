6a604bccde5c0541234b45a514ba14ed64932aa2
Mohamed Khidir Ageeb <ageeb1982@gmail.com>
2021-12-02 05:08:36 +0300

    add all img and all function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-import Quran from './books/quran.js';// console.log(Quran());
-import Hadith from './books/hadith.js';// console.log(Quran());
-import Aqida from './books/aqida.js';// console.log(Quran());
-console.log(Quran());
-console.log(Hadith());
+import Aqida from './books/aqida.js';
+import Arabic from './books/arabic.js';
+import Coctel from './books/coctel.js';
+import Figh from './books/figh.js';
+import Hadith from './books/hadith.js';
+import Quran from './books/quran.js';
+import Sera from './books/sera.js';
+import Tazkia from './books/tazkia.js';
+import Tragim from './books/tragim.js';
+
+
 console.log(Aqida());
+console.log(Arabic());
+console.log(Coctel());
+console.log(Figh());
+console.log(Hadith());
+console.log(Quran());
+console.log(Sera());
+console.log(Tazkia());
+console.log(Tragim());
+
